8387fd9d9eee42325adbf1ad4c9d7ed205a6e21a
Gabriel <50755276+GabrielRmartins@users.noreply.github.com>
2025-05-26 00:02:15 -0300

    creating some generic test to test github actions workflow

--- tests.py
+++ tests.py
@@ -1,1 +1,16 @@
+from github_actions_test import Point
 
+def test_generating_point():
+  a = Point(3,4)
+  assert a != None
+
+def test_point_parameters_initialization():
+  a = Point(3,4)
+  x,y = a.get_values()
+  assert x == 3
+  assert y == 4
+
+def test_distance_calculation():
+  a = Point(3,4)
+  b = Point(6,8)
+  assert a.calculate_distance(b) == 5
