2339777bdd6d1ac0d405c378624104141170c340
Gabriel <garoma20@gmail.com>
2025-06-03 04:04:14 -0300

    Updating DatabaseMangaer tests

--- src/db_manager.py
+++ src/db_manager.py
@@ -12,8 +12,6 @@ class DatabaseManager:
         self.connect()
 
     def connect(self):
-        if not os.path.exists(self.db_path):
-            raise FileNotFoundError(f"Database file {self.db_path} does not exist.")
         self.connection = sqlite3.connect(self.db_path)
         self.cursor = self.connection.cursor()
 
@@ -26,56 +24,73 @@ class DatabaseManager:
     def add_transaction(self, user:str , transaction: Transaction):
         if not self.cursor:
             raise RuntimeError("Database connection is not established.")
-        insert_query = f"INSERT INTO {user}_transactions (date, description, category, amount, type, id) VALUES ({transaction.get_date()}, {transaction.get_description()}, {transaction.get_category()}, {transaction.get_amount()}, {transaction.get_type()}, {transaction.get_id()})"
-        self.cursor.execute(insert_query)
+        query_parameters = (
+            transaction.get_date(),
+            transaction.get_description(),
+            transaction.get_category(),
+            transaction.get_amount(),
+            transaction.get_type(),
+            transaction.get_id()
+        )
+        insert_query = f"INSERT INTO {user} (date, description, category, amount, type, id) VALUES (?, ?, ?, ?, ?, ?)"
+        self.cursor.execute(insert_query, query_parameters)
         self.commit()
 
     def update_transaction_by_id(self, user: str, transaction_id: int, updated_transaction: Transaction):
         if not self.cursor:
             raise RuntimeError("Database connection is not established.")
-        update_query = f"UPDATE {user}_transactions SET date = {updated_transaction.get_date()}, description = {updated_transaction.get_description()}, category = {updated_transaction.get_category()}, amount = {updated_transaction.get_amount()}, type = {updated_transaction.get_type()} WHERE id = {transaction_id}"
-        self.cursor.execute(update_query, (*updated_transaction, transaction_id))
+        query_parameters = (
+            updated_transaction.get_date(),
+            updated_transaction.get_description(),
+            updated_transaction.get_category(),
+            updated_transaction.get_amount(),
+            updated_transaction.get_type(),
+            transaction_id
+            
+        )
+        update_query = f"UPDATE {user} SET date = ?, description = ?, category = ?, amount = ?, type = ? WHERE id = ?"
+        self.cursor.execute(update_query, query_parameters)
         self.commit()
     
     def delete_transaction_by_id(self, user: str, transaction_id: int):
         if not self.cursor:
             raise RuntimeError("Database connection is not established.")
-        delete_query = f"DELETE FROM {user}_transactions WHERE id = ?"
-        self.cursor.execute(delete_query, (transaction_id))
+        delete_query = f"DELETE FROM {user} WHERE id = ?"
+        self.cursor.execute(delete_query, (transaction_id,))
         self.commit()
 
     def get_category_transactions(self, user: str, category: str):
         if not self.cursor:
             raise RuntimeError("Database connection is not established.")
-        select_query = f"SELECT * FROM {user}_transactions WHERE category = {category}"
+        select_query = f"SELECT * FROM {user} WHERE category = '{category}'"
         self.cursor.execute(select_query)
         return self.cursor.fetchall()
     
     def get_all_transactions(self, user: str):
         if not self.cursor:
             raise RuntimeError("Database connection is not established.")
-        select_query = f"SELECT * FROM {user}_transactions"
+        select_query = f"SELECT * FROM {user}"
         self.cursor.execute(select_query)
         return self.cursor.fetchall()
     
     def get_all_debits(self, user: str):
         if not self.cursor:
             raise RuntimeError("Database connection is not established.")
-        select_query = f"SELECT * FROM {user}_transactions WHERE type = 'Despesa'"
+        select_query = f"SELECT * FROM {user} WHERE type = 'Despesa'"
         self.cursor.execute(select_query)
         return self.cursor.fetchall()
     
     def get_all_credits(self, user: str):
         if not self.cursor:
             raise RuntimeError("Database connection is not established.")
-        select_query = f"SELECT * FROM {user}_transactions WHERE type = 'Receita'"
+        select_query = f"SELECT * FROM {user} WHERE type = 'Receita'"
         self.cursor.execute(select_query)
         return self.cursor.fetchall()
     
     def get_month_transactions(self, user: str, month: int, year: int):
         if not self.cursor:
             raise RuntimeError("Database connection is not established.")
-        select_query = f"SELECT * FROM {user}_transactions WHERE strftime('%m', date) = '{month:02d}' AND strftime('%Y', date) = '{year}'"
+        select_query = f"SELECT * FROM {user} WHERE strftime('%m', date) = '{month:02d}' AND strftime('%Y', date) = '{year}'"
         self.cursor.execute(select_query)
         return self.cursor.fetchall()
 
@@ -87,7 +102,7 @@ class DatabaseManager:
     def check_username_availability(self, user: str) -> bool:
         if not self.cursor:
             raise RuntimeError("Database connection is not established.")
-        check_username_availability_query = f"SELECT name FROM sqlite_master WHERE type='table' AND name='{user}_transactions'"
+        check_username_availability_query = f"SELECT name FROM sqlite_master WHERE type='table' AND name='{user}'"
         self.cursor.execute(check_username_availability_query)
         return self.cursor.fetchone() is None
 
@@ -96,6 +111,6 @@ class DatabaseManager:
             raise RuntimeError("Database connection is not established.")
         if not self.check_username_availability(user):
             raise ValueError(f"Username '{user}' already exists.")
-        create_table_query = f"CREATE TABLE IF NOT EXISTS {user}_transactions (date, description, category, amount, type, id)"
+        create_table_query = f"CREATE TABLE IF NOT EXISTS {user} (date, description, category, amount, type, id)"
         self.cursor.execute(create_table_query)
         self.commit()
--- tests/DatabaseManager_test.py
+++ tests/DatabaseManager_test.py
@@ -3,3 +3,126 @@ from src.transactions import Transaction
 from src.transaction_type import TransactionType
 from datetime import date
 import pytest
+import os
+
+@pytest.fixture
+def db_manager():
+    db_path = 'tmp/test_database.db'
+    db_manager = DatabaseManager(db_path)
+    yield db_manager
+    db_manager.close()
+    if os.path.exists(db_path):
+        os.remove(db_path)
+
+
+
+class TestDatabaseManager:
+
+    def test_create_db_manager(self,db_manager):
+        assert db_manager.db_path == 'tmp/test_database.db'
+        assert db_manager.connection is not None
+        assert db_manager.cursor is not None
+        
+    def test_create_table(self, db_manager):
+        db_manager.create_user_table("test_user")
+        assert db_manager.check_username_availability("test_user") == False
+
+    def test_add_transaction(self, db_manager):
+        transaction = Transaction(date(2023, 10, 1), "Test Transaction", "Test Category", 100.0, TransactionType('Receita'), 1)
+        db_manager.create_user_table("test_user")
+        db_manager.add_transaction("test_user", transaction)
+        transactions = db_manager.get_all_transactions("test_user")
+        assert len(transactions) == 1
+        assert transactions[0][1] == "Test Transaction"
+        assert transactions[0][2] == "Test Category"
+        assert transactions[0][3] == 100.0
+        assert transactions[0][4] == "Receita"   
+
+    def test_update_transaction(self, db_manager):
+        transaction = Transaction(date(2023, 10, 1), "Test Transaction", "Test Category", 100.0, TransactionType('Receita'), 1)
+        db_manager.create_user_table("test_user")
+        db_manager.add_transaction("test_user", transaction)
+        
+        updated_transaction = Transaction(date(2023, 10, 2), "Updated Transaction", "Updated Category", 200.0, TransactionType('Despesa'), 1)
+        db_manager.update_transaction_by_id("test_user", 1, updated_transaction)
+        
+        transactions = db_manager.get_all_transactions("test_user")
+        assert len(transactions) == 1
+        assert transactions[0][1] == "Updated Transaction"
+        assert transactions[0][2] == "Updated Category"
+        assert transactions[0][3] == 200.0
+        assert transactions[0][4] == "Despesa"
+        assert transactions[0][5] == 1
+
+    def test_delete_transaction(self, db_manager):
+        transaction = Transaction(date(2023, 10, 1), "Test Transaction", "Test Category", 100.0, TransactionType('Receita'), 1)
+        db_manager.create_user_table("test_user")
+        db_manager.add_transaction("test_user", transaction)
+        
+        db_manager.delete_transaction_by_id("test_user", 1)
+        
+        transactions = db_manager.get_all_transactions("test_user")
+        assert len(transactions) == 0
+
+    def test_get_category_transactions(self, db_manager):
+        transaction1 = Transaction(date(2023, 10, 1), "Test Transaction 1", "Test Category", 100.0, TransactionType('Receita'), 1)
+        transaction2 = Transaction(date(2023, 10, 2), "Test Transaction 2", "Test Category", 200.0, TransactionType('Despesa'), 2)
+        db_manager.create_user_table("test_user")
+        db_manager.add_transaction("test_user", transaction1)
+        db_manager.add_transaction("test_user", transaction2)
+        
+        category_transactions = db_manager.get_category_transactions("test_user", "Test Category")
+        assert len(category_transactions) == 2
+        assert category_transactions[0][1] == "Test Transaction 1"
+        assert category_transactions[1][1] == "Test Transaction 2"
+
+    def test_get_all_transactions(self, db_manager):
+        transaction1 = Transaction(date(2023, 10, 1), "Test Transaction 1", "Test Category", 100.0, TransactionType('Receita'), 1)
+        transaction2 = Transaction(date(2023, 10, 2), "Test Transaction 2", "Test Category 2", 200.0, TransactionType('Despesa'), 2)
+        db_manager.create_user_table("test_user")
+        db_manager.add_transaction("test_user", transaction1)
+        db_manager.add_transaction("test_user", transaction2)
+        
+        transactions = db_manager.get_all_transactions("test_user")
+        assert len(transactions) == 2
+        assert transactions[0][1] == "Test Transaction 1"
+        assert transactions[1][1] == "Test Transaction 2"
+
+    def test_get_all_debits(self, db_manager):
+        transaction1 = Transaction(date(2023, 10, 1), "Debit Transaction 1", "Test Category", 100.0, TransactionType('Despesa'), 1)
+        transaction2 = Transaction(date(2023, 10, 2), "Credit Transaction 1", "Test Category", 200.0, TransactionType('Receita'), 2)
+        db_manager.create_user_table("test_user")
+        db_manager.add_transaction("test_user", transaction1)
+        db_manager.add_transaction("test_user", transaction2)
+        
+        debits = db_manager.get_all_debits("test_user")
+        assert len(debits) == 1
+        assert debits[0][1] == "Debit Transaction 1"
+        assert debits[0][4] == "Despesa"
+
+    def test_get_all_credits(self, db_manager):
+        transaction1 = Transaction(date(2023, 10, 1), "Credit Transaction 1", "Test Category", 100.0, TransactionType('Receita'), 1)
+        transaction2 = Transaction(date(2023, 10, 2), "Debit Transaction 1", "Test Category", 200.0, TransactionType('Despesa'), 2)
+        db_manager.create_user_table("test_user")
+        db_manager.add_transaction("test_user", transaction1)
+        db_manager.add_transaction("test_user", transaction2)
+        
+        credits = db_manager.get_all_credits("test_user")
+        assert len(credits) == 1
+        assert credits[0][1] == "Credit Transaction 1"
+        assert credits[0][4] == "Receita"
+        
+    def test_get_month_transactions(self, db_manager):
+        transaction1 = Transaction(date(2023, 10, 1), "Transaction October 1", "Test Category", 100.0, TransactionType('Receita'), 1)
+        transaction2 = Transaction(date(2023, 11, 1), "Transaction November 1", "Test Category", 200.0, TransactionType('Despesa'), 2)
+        db_manager.create_user_table("test_user")
+        db_manager.add_transaction("test_user", transaction1)
+        db_manager.add_transaction("test_user", transaction2)
+        
+        october_transactions = db_manager.get_month_transactions("test_user", 10, 2023)
+        assert len(october_transactions) == 1
+        assert october_transactions[0][1] == "Transaction October 1"
+        
+        november_transactions = db_manager.get_month_transactions("test_user", 11, 2023)
+        assert len(november_transactions) == 1
+        assert november_transactions[0][1] == "Transaction November 1"
